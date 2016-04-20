16b50161222891592c22ed9e2191ac32d375c54f
Awen <Awentljs@gmail.com>
2016-04-20 12:03:35 +0800

    photopick

---
+++ app/src/main/java/com/awen/photo/ImageProvider.java
@@ -0,0 +1,35 @@
+package com.awen.photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Awen <Awentljs@gmail.com>
+ */
+public class ImageProvider {
+
+    public static ArrayList<String> getImageUrls(){
+        ArrayList<String> list = new ArrayList<>();
+        //也可以传本地图片，格式如下：
+//                list.add("file:///storage/emulated/0/frgdr/frgdr_Video/im_101460431984904.jpg");
+//                list.add("file:///storage/emulated/0/frgdr/frgdr_Video/im_101460432332951.jpg");
+        list.add("http://p1.wmpic.me/article/2015/06/29/1435559754_fnZtksvI.jpg");
+        list.add("http://p2.wmpic.me/article/2015/06/29/1435559758_rMcNLvQq.jpg");
+        list.add("http://pic1.win4000.com/pic/6/da/50bc520323.jpg");
+        list.add("http://www.wmpic.me/wp-content/uploads/2014/02/20140218150739227.jpg");
+        list.add("http://www.bz55.com/uploads/allimg/140904/138-140Z4092036.jpg");
+        list.add("http://image.hiapk.com/pic/2012/01/10/and_pbpic_qunjie20120110_14.jpg");
+        list.add("http://img.article.pchome.net/game/00/11/14/22/pic_lib/wm/Yuurimorishita16.jpg");
+        list.add("http://beiwoo.cn/uploads/allimg/120621/1106001S4-3.jpg");
+        list.add("http://pic2.qnpic.com/doimg/meiyuanguan/9bda3e55/");
+        list.add("http://img.weibonvren.com/meinv/qita/senxia/36.jpg");
+        list.add("http://www.winddesktop.com/Wallpaper/01563/Original/01563_001_3Ik8vH1i.jpg");
+        list.add("http://img.yxbao.com/news/image/201503/23/61ebe76486.jpg");
+        list.add("http://img1.gtimg.com/ent/pics/hv1/142/234/1470/95646562.jpg");
+        list.add("http://www.weimeitupian.com/wp-content/uploads/2015/02/20150215145914389.jpg");
+        list.add("http://d.3987.com/xglsnymnipadzmbz.120906/005.jpg");
+        list.add("http://www.7zhan.com/uploads/allimg/131007/8-13100F91118.jpg");
+        list.add("http://pic2.qnpic.com/doimg/meiyuanguan/9bda3e55/");
+        return list;
+    }
+}
